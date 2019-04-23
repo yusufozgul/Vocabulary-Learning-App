3f7e28bb30b1383c62359ada0012ae6bf1ad4928
yusuf özgül <yozgul@icloud.com>
2019-04-23 11:43:30 +0300

    settings Project

--- VocabularyLearningApp/VocabularyLearningApp/VocabularyLearningApp/Controllers/LearnVC.swift
+++ VocabularyLearningApp/VocabularyLearningApp/VocabularyLearningApp/Controllers/LearnVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class LearnVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- VocabularyLearningApp/VocabularyLearningApp/VocabularyLearningApp/Controllers/TestVC.swift
+++ VocabularyLearningApp/VocabularyLearningApp/VocabularyLearningApp/Controllers/TestVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class TestVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
